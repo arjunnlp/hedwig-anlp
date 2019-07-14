08436da8874e4207d2cb0ff5e54583beaf18c541
Dainis Boumber <dainis.boumber@gmail.com>
2019-07-13 23:20:21 -0500

    Fork versioning away from original

--- hedwig/setup.py
+++ hedwig/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hedwig',
-      version='1.1.0',
+      version='2.0.0',
       description='PyTorch deep learning models for document classification',
       packages=['char_cnn', 'han', 'kim_cnn', 'reg_lstm', 'xml_cnn', 'bert', 'hbert'],
       )
